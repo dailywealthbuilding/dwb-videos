b818a197a1447eee31dbf2d75d7504924e537081
dailywealthbuilding <daily.wealthbuilding@gmail.com>
2026-03-28 08:32:13 +0300

    Create remotion.config.js

---
+++ remotion.config.js
@@ -0,0 +1,11 @@
+import { Config } from '@remotion/cli/config';
+
+Config.setVideoImageFormat('jpeg');
+Config.setOverwriteOutput(true);
+Config.setPixelFormat('yuv420p');
+Config.setCodec('h264');
+Config.setCrf(18);
+
+Config.setWidth(1080);
+Config.setHeight(1920);
+Config.setFps(30);
