25bf5c8a62d2f8c7ac9bcd5e9b3a4b6c7acaea1e
dailywealthbuilding <daily.wealthbuilding@gmail.com>
2026-03-28 08:57:52 +0300

    Update remotion.config.js

--- remotion.config.js
+++ remotion.config.js
@@ -5,7 +5,3 @@ Config.setOverwriteOutput(true);
 Config.setPixelFormat('yuv420p');
 Config.setCodec('h264');
 Config.setCrf(18);
-
-Config.setWidth(1080);
-Config.setHeight(1920);
-Config.setFps(30);
